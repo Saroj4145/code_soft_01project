a0f07f2f4c465afdf66443ed946b60578192271c
Saroj4145 <kumarsaroj7847@gmail.com>
2024-06-14 20:16:25 +0530

    Add files via upload

---
+++ GradeCalculator.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        int[] marks = new int[numSubjects];
+        System.out.println("Enter marks for each subject (out of 100):");
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+
+        int totalMarks = calculateTotalMarks(marks);
+        double averagePercentage = calculateAveragePercentage(totalMarks, numSubjects);
+        String grade = assignGrade(averagePercentage);
+
+        displayResults(totalMarks, averagePercentage, grade);
+    }
+
+    static int calculateTotalMarks(int[] marks) {
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        return totalMarks;
+    }
+
+    static double calculateAveragePercentage(int totalMarks, int numSubjects) {
+        return (double) totalMarks / numSubjects;
+    }
+
+    static String assignGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B";
+        } else if (averagePercentage >= 60) {
+            return "C";
+        } else if (averagePercentage >= 50) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+    static void displayResults(int totalMarks, double averagePercentage, String grade) {
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage);
+        System.out.println("Grade: " + grade);
+    }
+}
